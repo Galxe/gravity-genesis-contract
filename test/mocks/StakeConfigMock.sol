93ab71f8e72f6ec14026cf78d4f22d25a5110fc1
ByteYue <yj976240184@gmail.com>
2025-07-22 14:05:02 +0800

    first

--- test/mocks/StakeConfigMock.sol
+++ test/mocks/StakeConfigMock.sol
@@ -3,8 +3,18 @@ pragma solidity 0.8.30;
 
 contract StakeConfigMock {
     bool public initialized;
+    uint256 public votingPowerIncreaseLimit;
+    uint256 public MAX_COMMISSION_RATE;
 
     function initialize() external {
         initialized = true;
+    }
+
+    function setVotingPowerIncreaseLimit(uint256 _limit) external {
+        votingPowerIncreaseLimit = _limit;
+    }
+
+    function setMAX_COMMISSION_RATE(uint256 _rate) external {
+        MAX_COMMISSION_RATE = _rate;
     }
 }
